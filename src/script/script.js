c371afdff5c616cfba12ab1d698fbdad5b7f20fa
Athul Krishna P S <athulkrishna174@gmail.com>
2024-01-15 00:33:13 +0530

    added sections

--- src/script/script.js
+++ src/script/script.js
@@ -6,9 +6,26 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        }
+    });
 }
 
 document.querySelector('#search-icon').onclick = () =>{
@@ -18,3 +35,17 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
